da53051953e7cfc0c61e791b0386936869ac1c0e
Grazy0range <76399929+Grazy0range@users.noreply.github.com>
2023-04-10 06:53:12 +0300

    123

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        nav:true,
+        center: true,
+        autoWidth:true,
+        items:3,
+        margin:1250,
+        dots: true,
+    });
+  });
