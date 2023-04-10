30d87b5c7dc95ee9ff567c4bbf92a8c21020563e
Grazy0range <76399929+Grazy0range@users.noreply.github.com>
2023-04-10 12:56:16 +0300

    123

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
+    $(".gallery__slider").owlCarousel({
         loop:true,
         nav:true,
         center: true,
         autoWidth:true,
         items:3,
         margin:1250,
+        dots: true,
+    });
+  });
+
+$(document).ready(function(){
+    $(".hall__slider").owlCarousel({
+        loop:true,
+        nav:false,
+        center:true,
+        items:1,
+        margin:0,
+        dots: true,
         dots: true,
     });
   });
